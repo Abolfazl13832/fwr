ecd16cd3293407c6d3cecc7ae51d44ef16d27c60
ابوالفضل علی پور <alypwr573@gmail.com>
2024-08-26 20:58:35 +0330

    fixer pack

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="fixer",
+    version="1.0",
+    packages=['fixer'],
+    requires = ['json','requests'],
+    url='https://gitlab.com/wizard4081230/abolfazl2.git'
+
+)
